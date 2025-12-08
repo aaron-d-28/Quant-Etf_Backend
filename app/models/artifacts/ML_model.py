08711e24500dc3d6ccfb01c636ebe7c3041bb67b
aaron-d-28 <dsouzaaaron28@gmail.com>
2025-12-08 18:11:46 +0530

    Added changes to ml model

--- app/models/artifacts/ML_model.py
+++ app/models/artifacts/ML_model.py
@@ -2,20 +2,20 @@ import pandas as pd
 from tensorflow import keras
 import pickle as pkl
 
-modelpath = "./Riskmodel_dense.keras"
+modelpath = "./Riskmodel_Dense.keras"
 encoderpath ="../../MainApp/app/models/artifacts/encoder.pkl"
 model = keras.models.load_model(modelpath)
 encoder =pkl.load(open(encoderpath, "rb"))
 
 def preprocess(df: pd.DataFrame,ticker_column='ticker'):
-
+    colstodrop=['date','month','rank','ticker','return_p','sharpe_p','vol_p','downside_p','var_p','cvar_p','dd_p']
     ticker_encoded = encoder.transform(df[[ticker_column]])
 
     ticker_df = pd.DataFrame(ticker_encoded,
                              columns=encoder.get_feature_names_out([ticker_column]),
                              index=df.index
                              )
-    df = df.drop(columns=[ticker_column,'date','month','rank'], errors='ignore')
+    df = df.drop(columns=colstodrop, errors='ignore')
     df = pd.concat([df,ticker_df], axis=1)
     return df
 
